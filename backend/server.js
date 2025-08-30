fee4e4c2b3d7ebfe6c2eb7b1e7434cd1b6ef9886
Raihan Muhammad Riswandi <165734611+Raihan220106@users.noreply.github.com>
2025-08-30 14:30:06 +0700

    server.js

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,12 @@ const bodyParser = require("body-parser");
 const mysql = require("mysql2");
 
 const app = express();
-const PORT = 5000; 
+const PORT = 4000; 
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-const path = require("path");
-app.use(express.static(path.join(__dirname, "../frontend")));
+app.use(express.static("../frontend"));
 
 // Database connection
 const db = mysql.createConnection({
